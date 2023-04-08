421c157b9e34e01ba8870fe06ae2af777f135926
sopand <pows1031@naver.com>
2023-04-08 15:17:34 +0900

    04-08 Member 이메일인증 수정

--- src/main/java/com/example/shop/service/EmailService.java
+++ src/main/java/com/example/shop/service/EmailService.java
@@ -14,11 +14,16 @@ import org.springframework.stereotype.Service;
 import java.io.UnsupportedEncodingException;
 import java.util.Random;
 
+
+/**
+ * 이메일을 보내기위한 기능을 담당하는 서비스클래스
+ */
 @PropertySource("classpath:application.yml")
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class EmailService {
+
 
    private final JavaMailSender javaMailSender; // ** JavaMailSender emailSender;//** 의존성 주입을 통해서 필요한 객체를 가져온다.
 
@@ -26,12 +31,20 @@ public class EmailService {
 
    @Value("${spring.mail.username}")
    private String id;
-
+   
+   
    //String keyword로 쓰면 여기서 임시비밀번호도 지정가능!!!
+
+   /**
+    * 메일을 만들어주는 기능을 담당하는 로직
+    * @param to = 메시지를 보려고하는 사용자의 이메일
+    * @param ePw = createKey메서드에서 만들어진 인증코드
+    * @return = 만들어진 메시지를 리턴
+    * @throws MessagingException
+    * @throws UnsupportedEncodingException
+    */
    public MimeMessage createMessage(String to,String ePw) throws MessagingException, UnsupportedEncodingException {
 
-      System.out.println("보내는 대상 : " + to);
-      System.out.println("인증번호 : " + ePw);
       MimeMessage message = javaMailSender.createMimeMessage();
 
       message.addRecipients(MimeMessage.RecipientType.TO, to); // to 보내는 대상
@@ -48,12 +61,15 @@ public class EmailService {
       return message;
    }
 
-   // ** @Override
-   // 인증코드 만들기
+
+   /**
+    * 메시지에 보내기 위한 인증코드를 작성해주는 로직
+    * @return = 랜덤하게 만들어진  8자리의 인증코드를 리턴
+    */
    public String createKey() { // **public void createCode() {
       String key = "";
       Random rnd = new Random(); // * Random random = new Random();
-      for (int i = 0; i < 8; i++) { // 인증코드 6자리
+      for (int i = 0; i < 8; i++) { // 인증코드 8자리
          key += String.valueOf(rnd.nextInt(10));
       }
       return String.valueOf(key);
@@ -65,9 +81,7 @@ public class EmailService {
     * 메일의 내용을 담아준다. bean으로 등록해둔 javaMailSender 객체를 사용하여 이메일 send
     */
    public String sendSimpleMessage(String to) throws Exception {
-      String ePw = "";
-      ePw = createKey();
-      // ** ePw=createKey(); 이렇게 치면 전역.지역변수로 에러가 나는 듯..? 민규는 맨 위에 선언해둠!
+      String ePw = createKey();
       MimeMessage message = createMessage(to, ePw);
       try {
          javaMailSender.send(message); // 메일 발송
